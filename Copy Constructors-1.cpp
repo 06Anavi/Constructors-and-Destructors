60fcd7e80ee9b5826cfa52ad83f60fc693691ce3
06Anavi <anvik0105@gmail.com>
2025-09-01 20:03:14 +0530

    Create Copy Constructors-1.cpp

---
+++ Copy Constructors-1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Date {
+    int day, month, year;
+public:
+    Date(int d, int m, int y) {
+        day = d;
+        month = m;
+        year = y;
+    }
+    copyDate(Date &d) { 
+        day = d.day;
+        month = d.month;
+        year = d.year;
+    }
+    void displayDate() {
+        cout << day << "/" << month << "/" << year << endl;
+    }
+};  
+
+int main() {
+    Date d1(28,8,2025);
+    Date d2(d1); 
+    cout << "Original Date: ";
+    d1.displayDate();
+    cout << "Copied Date: ";
+    d2.displayDate();
+
+    return 0;
+}
+
+/* OUTPUT:
+Original Date: 28/8/2025
+Copied Date: 28/8/2025
+
+*/
